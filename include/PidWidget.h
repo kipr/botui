3b6048a965aa4cecd361a72be1bd5c8ee5cf183b
Braden McDorman <bmcdorman@gmail.com>
2012-12-22 16:51:14 -0600

    Really added the PID widget

---
+++ include/PidWidget.h
@@ -0,0 +1,31 @@
+#ifndef _PIDWIDGET_H_
+#define _PIDWIDGET_H_
+
+#include "StandardWidget.h"
+
+namespace Ui
+{
+	class PidWidget;
+}
+
+class QPushButton;
+
+class PidWidget : public StandardWidget
+{
+Q_OBJECT
+public:
+	PidWidget(Device *device, QWidget *parent = 0);
+	~PidWidget();
+	
+private slots:
+	void portClicked();
+	void go();
+	void statusUpdate();
+	
+private:
+	Ui::PidWidget *ui;
+	QPushButton *m_ports[4];
+	quint8 m_currentPort;
+};
+
+#endif
---
+++ src/PidWidget.cpp
@@ -0,0 +1,71 @@
+#include "PidWidget.h"
+#include "ui_PidWidget.h"
+
+#include "ArrayHelpers.h"
+#include "NumpadDialog.h"
+
+#include <kovan/general.h>
+#include <kovan/motors.h>
+
+#include <QTimer>
+#include <QTimer>
+
+PidWidget::PidWidget(Device *device, QWidget *parent)
+	: StandardWidget(device, parent),
+	ui(new Ui::PidWidget)
+{
+	ui->setupUi(this);
+	
+	m_ports[0] = ui->_0;
+	m_ports[1] = ui->_1;
+	m_ports[2] = ui->_2;
+	m_ports[3] = ui->_3;
+	
+	ui->position->setInputProvider(new NumpadDialog(tr("Update Motor Position"),
+		NumpadDialog::Integer,
+		std::numeric_limits<short>::min(),
+		std::numeric_limits<short>::max(),
+		this));
+	
+	ui->speed->setMinimumValue(-4096);
+	ui->speed->setMaximumValue(4095);
+	ui->speed->setValue(0.0);
+	
+	connect(ui->go, SIGNAL(clicked()), SLOT(go()));
+	
+	for(quint8 i = 0; i < sizeof_array(m_ports); ++i) {
+		connect(m_ports[i], SIGNAL(clicked()), SLOT(portClicked()));
+	}
+}
+
+PidWidget::~PidWidget()
+{
+	delete ui;
+}
+
+void PidWidget::portClicked()
+{
+	QObject *from = sender();
+	if(!from) return;
+	
+	m_currentPort = 0;
+	for(; m_currentPort < sizeof_array(m_ports); ++m_currentPort) {
+		if(m_ports[m_currentPort] == from) break;
+	}
+	
+	for(quint8 j = 0; j < sizeof_array(m_ports); ++j) {
+		m_ports[j]->setEnabled(j != m_currentPort);
+	}
+}
+
+void PidWidget::go()
+{
+	int position = ui->position->text().toInt();
+	move_to_position(m_currentPort, position, ui->speed->value());
+}
+
+void PidWidget::statusUpdate()
+{
+	publish();
+	ui->status->setText("???");
+}
