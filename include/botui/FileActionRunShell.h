d5dd586199e5fa9c65d0a5fdfe92d4f2e4de8ac7
Braden McDorman <bmcdorman@gmail.com>
2015-04-15 20:10:04 -0500

    Added run shell

---
+++ include/botui/FileActionRunShell.h
@@ -0,0 +1,13 @@
+#ifndef _FILEACTIONRUNSHELL_H_
+#define _FILEACTIONRUNSHELL_H_
+
+#include "FileActions.h"
+
+class FileActionRunShell : public FileActionExtension
+{
+public:
+	FileActionRunShell();
+	virtual bool act(const QString &path, Device *device) const;
+};
+
+#endif
---
+++ src/FileActionRunShell.cpp
@@ -0,0 +1,21 @@
+#include "FileActionInstallPkg.h"
+#include "RootController.h"
+#include "ProcessLogDialog.h"
+
+#include <QDebug>
+
+FileActionRunShell::FileActionRunShell()
+	: FileActionExtension(QObject::tr("Run"), QStringList() << "sh" << "bash")
+{
+	
+}
+
+bool FileActionInstallPkg::act(const QString &path, Device *device) const
+{
+  ProcessLogDialog dialog;
+  if(!dialog.start("/bin/sh", QStringList() << path)) return false;
+	RootController::ref().presentDialog(&dialog);
+	return true;
+}
+
+REGISTER_FILE_ACTION(FileActionRunShell)
