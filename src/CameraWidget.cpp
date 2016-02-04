79c5514bd86a0c2a4c47f621991a98213e0cebb6
Nafis Zaman <nafis.zaman-1@ou.edu>
2016-02-04 05:06:33 -0600

    Handle connect/disconnect in camera views
    Related to #8

--- src/CameraWidget.cpp
+++ src/CameraWidget.cpp
@@ -52,28 +52,43 @@ bool CameraWidget::trackBlobs() const
 // TODO: Make it configurable which channels to show
 void CameraWidget::update()
 {
-  // TODO: Gracefully handle camera connections (retry, lower fps, etc.)
-  if(!isVisible()) return;
-  if(!m_camDevice->update()) {
-    qWarning() << "camera update failed";
-    return;
+  if(!m_camDevice->isOpen()) {
+    // Camera isn't open, so try to open
+    if(!m_camDevice->open()) {
+      // Camera not available; lower frame rate and quit
+      // TODO: lower framerate
+      return;
+    }
+    
+    // Successfully opened camera; raise frame rate
+    // TODO: raise framerate
   }
-  qWarning() << "camera updated";
   
-  cv::Mat image = m_camDevice->rawImage();
-  if(m_trackBlobs) {
-    int h = 0;
-    const static int hStep = 137; // Golden angle
-    Camera::ChannelPtrVector::const_iterator it = m_camDevice->channels().begin();
-    for(; it != m_camDevice->channels().end(); ++it, h += hStep) {
-      const QColor rectColor = QColor::fromHsv(h % 360, 255, 255);
-      const Camera::ObjectVector *objs = (*it)->objects();
-      Camera::ObjectVector::const_iterator oit = objs->begin();
-      for(; oit != objs->end(); ++oit) {
-        const Camera::Object &obj = *oit;
-        cv::rectangle(image, cv::Rect(obj.boundingBox().x(), obj.boundingBox().y(),
-          obj.boundingBox().width(), obj.boundingBox().height()),
-          cv::Scalar(rectColor.red(), rectColor.blue(), rectColor.blue()), 2);
+  if(!isVisible()) return;
+  
+  cv::Mat image;
+  if(!m_camDevice->update()) {
+    // Update failed, so close camera and lower frame rate
+    m_camDevice->close();
+    // TODO: lower framerate
+  }
+  else {
+    qDebug() << "Camera updated!";
+    image = m_camDevice->rawImage();
+    if(m_trackBlobs) {
+      int h = 0;
+      const static int hStep = 137; // Golden angle
+      Camera::ChannelPtrVector::const_iterator it = m_camDevice->channels().begin();
+      for(; it != m_camDevice->channels().end(); ++it, h += hStep) {
+        const QColor rectColor = QColor::fromHsv(h % 360, 255, 255);
+        const Camera::ObjectVector *objs = (*it)->objects();
+        Camera::ObjectVector::const_iterator oit = objs->begin();
+        for(; oit != objs->end(); ++oit) {
+          const Camera::Object &obj = *oit;
+          cv::rectangle(image, cv::Rect(obj.boundingBox().x(), obj.boundingBox().y(),
+            obj.boundingBox().width(), obj.boundingBox().height()),
+            cv::Scalar(rectColor.red(), rectColor.blue(), rectColor.blue()), 2);
+        }
       }
     }
   }
