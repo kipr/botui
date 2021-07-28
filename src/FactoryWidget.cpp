98394f3f242b8d05f554761bede4fe7f6f4d1a14
Zachary Sasser <ztsasser@gmail.com>
2021-07-28 12:20:23 -0500

    process events

--- src/FactoryWidget.cpp
+++ src/FactoryWidget.cpp
@@ -85,7 +85,7 @@ void FactoryWidget::reflash()
     ui->changeSerialLabel->setText("Flash Progress:");
 
     //Force Redraw
-    processEvents();
+    QCoreApplication::processEvents()
 
     //Setup QProcess
     m_consoleProc = new QProcess();
@@ -121,7 +121,7 @@ void FactoryWidget::experimental()
     ui->changeSerialLabel->setText("Experimental Install:");
 
     //Force Redraw
-    processEvents();
+    QCoreApplication::processEvents()
 
     //Setup QProcess
     m_consoleProc = new QProcess();
