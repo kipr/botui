b61d0a76465aee003ae50d3b54190d97d20a65b0
Zachary Sasser <ztsasser@gmail.com>
2021-08-05 15:23:07 -0500

    Moved failure

--- src/WallabyUpdateWidget.cpp
+++ src/WallabyUpdateWidget.cpp
@@ -114,7 +114,7 @@ void WallabyUpdateWidget::updateFinished(int, QProcess::ExitStatus exitStatus)
     else{
         ui->updateConsole->insertPlainText("Update Failed (Crashed):\n" +
                                            "The update script has crashed with an error. \n" +
-                                           "Contact KIPR tech support for assistance");
+                                           "Contact KIPR tech support for assistance if the problem persists \n");
     }
 
   // Cleanup process
@@ -129,12 +129,7 @@ void WallabyUpdateWidget::updateFinished(int, QProcess::ExitStatus exitStatus)
   ui->refresh->setEnabled(true);
   ui->update->setEnabled(true);
 
-  //If the program gets to this point without rebooting, the update has probably failed.
-  ui->updateConsole->insertPlainText("Update Failed (Timed Out): \n " +
-                                     "The controller should have rebooted after it finished the update script. \n" +
-                                     "If you are updating over internet, double check the connection and try again (it takes a few seconds to connect). \n" +
-                                     "If you are updating over flash drive, your drive might be configured wrong. \n" +
-                                     "If you are still having trouble, contact KIPR tech support or open an issue at github.com/kipr/botui \n")
+
 }
 
 bool WallabyUpdateWidget::mountUsb(const QString device, const QDir dir)
@@ -169,6 +164,15 @@ void WallabyUpdateWidget::ethernet(){
       ui->updateConsole->setProcess(m_updateProc);
       connect(m_updateProc, SIGNAL(finished(int, QProcess::ExitStatus)), SLOT(updateFinished(int, QProcess::ExitStatus)));
       m_updateProc->start("sudo sh /home/pi/updateMe.sh");
+
+      if(m_updateProc->state() == QProcess::NotRunning){
+          //If the program gets to this point without rebooting, the update has probably failed.
+          ui->updateConsole->insertPlainText("Update Failed (Non-start/Timeout): \n " +
+                                             "The controller should have rebooted after it finished the update script. \n" +
+                                             "If you are updating over internet, double check the connection and try again (it takes a few seconds to connect). \n" +
+                                             "If you are updating over flash drive, your drive might be configured wrong. \n" +
+                                             "If you are still having trouble, contact KIPR tech support or open an issue at github.com/kipr/botui \n")
+      }
 
 }
 
