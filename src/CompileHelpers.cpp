24c7826bb01ac47b424f60a90776acf2d62e47da
Erin Harrington <erinharrington-12@yahoo.com>
2022-08-29 14:56:51 -0500

    Continued fixing depracated functions (indexIn --> globalMatch, etc)

--- src/CompileHelpers.cpp
+++ src/CompileHelpers.cpp
@@ -1,18 +1,31 @@
 #include "CompileHelpers.h"
 
 #include <QFileInfo>
-#include <QRegExp>
+#include <QRegularExpression>
 
 QString CompileHelpers::postProcess(const QString &output)
 {
 	QString ret = output;
-	QRegExp rx("([a-zA-Z]:)?(/[a-zA-Z0-9_.-]+)*([a-zA-Z0-9_-]+\\.[a-zA-Z]+)/?:");
-	int pos = 0;
-	while((pos = rx.indexIn(ret, pos)) != -1) {
-		const QString path = ret.mid(pos, rx.matchedLength() - 1);
+	QRegularExpression rx("([a-zA-Z]:)?(/[a-zA-Z0-9_.-]+)*([a-zA-Z0-9_-]+\\.[a-zA-Z]+)/?:");
+	QRegularExpressionMatchIterator i = rx.globalMatch(ret);
+	
+	while(i.hasNext()) {
+		QRegularExpressionMatch match = i.next();
+		const QString path = match.captured(1); //get the first substring that matched rx
 		const QString ins = QFileInfo(path).fileName();
-		ret.replace(pos, path.length(), ins);
-		pos += ins.length();
+
+		int startOffset = match.capturedStart(1);
+		ret.replace(startOffset, path.length(), ins);
+		
 	}
 	return ret;
 }
+
+
+// int pos = 0;
+// 	while((pos = rx.indexIn(ret, pos)) != -1) {
+// 		const QString path = ret.mid(pos, rx.matchedLength() - 1);
+// 		const QString ins = QFileInfo(path).fileName();
+// 		ret.replace(pos, path.length(), ins);
+// 		pos += ins.length();
+// 	}
