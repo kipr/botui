55e1932d5ec8c9a47a1329d7b8585f817a1019a3
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-04-03 21:12:52 -0700

    move slider horizontally

--- src/TouchSlider.cpp
+++ src/TouchSlider.cpp
@@ -1,6 +1,9 @@
 #include "TouchSlider.h"
-#include <QDebug>
 
-TouchSlider::TouchSlider(QWidget *parent) : QSlider(parent), _label(0xFFFF){};
+TouchSlider::TouchSlider(QWidget *parent) : QSlider(Qt::Horizontal, parent), _label(0xFFFF)
+{
+    QString style_sheet = tr("QSlider { min-width: 100px; height: 40px; } .QSlider::groove:horizontal {border: 1px solid #111; border-radius: 10px; background-color: black; margin: 2px 0; } .QSlider::handle:horizontal { background-color: red; border: 1px solid red; width: 30px; margin: -2px 0; border-radius: 10px; } ");
+    this->setStyleSheet(style_sheet);
+};
 quint16 TouchSlider::label() { return _label; }
 void TouchSlider::setLabel(quint16 label) { _label = label; }
