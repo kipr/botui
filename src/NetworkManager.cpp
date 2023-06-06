6b8373eaf77fe7147f6758deb2857ee43495683a
chrehall68 <hallchristopher8697@students.esuhsd.org>
2023-06-06 10:06:19 -0700

    revert wifi device to wlo1

--- src/NetworkManager.cpp
+++ src/NetworkManager.cpp
@@ -49,7 +49,7 @@ Connection DEFAULT_AP;
 #define WIFI_DEVICE "wlan0" // always wlan0 for raspberry pi
 #endif
 
-#define WIFI_DEVICE "wlp3s0" // always wlan0 for raspberry pi
+#define WIFI_DEVICE "wlo1" // always wlan0 for raspberry pi
 #define AP_NAME m_dev->serial() + "-wombatAP"
 #define AP_SSID (AP_NAME).toUtf8()
 #define AP_PASSWORD SystemUtils::sha256(m_dev->id()).left(6) + "00"
