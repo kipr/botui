394b1d5357f8cfdedd452a4b7a1b7e412767a2c8
Erin <erinharrington12@yahoo.com>
2024-03-12 15:54:14 -0500

    Added TelloWidget.cpp for future implementation

---
+++ include/botui/TelloWidget.h
@@ -0,0 +1,32 @@
+#ifndef _TELLOWIDGET_H_
+#define _TELLOWIDGET_H_
+
+#include "StandardWidget.h"
+
+#include <QRegularExpression>
+#include <QProcess>
+
+namespace Ui
+{
+	class TelloWidget;
+}
+
+
+class TelloWidget : public StandardWidget
+{
+Q_OBJECT
+public:
+	TelloWidget(Device *device, QWidget *parent = 0);
+	~TelloWidget();
+
+private slots:
+
+
+private:
+	Ui::TelloWidget *ui;
+
+
+};
+
+
+#endif
---
+++ src/TelloWidget.cpp
@@ -0,0 +1,36 @@
+#include "TelloWidget.h"
+#include "ui_TelloWidget.h"
+#include "Create3SensorListWidget.h"
+#include "Create3ExampleWidget.h"
+#include "Create3SensorModel.h"
+#include <kipr/create3/client/client.h>
+#include <unistd.h>
+#include "NumpadDialog.h"
+#include <QDebug>
+#include <stdio.h>
+#include <iostream>
+#include <QRegularExpression>
+#include <QProcess>
+#include <QHeaderView>
+#include <cmath>
+#include <QTimer>
+#include <QPainter>
+#include <QItemDelegate>
+#include "RootController.h"
+
+
+TelloWidget::TelloWidget(Device *device, QWidget *parent)
+    : StandardWidget(device, parent),
+      ui(new Ui::TelloWidget)
+{
+    ui->setupUi(this);
+    performStandardSetup(tr("Tello"), false);
+    setStyleSheet("QScrollBar:vertical {border: 2px solid grey;background:grey ;width: 100px; margin: 22px 0 22px 0; } QScrollBar::handle:vertical { border: 2px solid grey;background: white; min-height: 20px; } QScrollBar::add-line:vertical { border: 2px solid grey;background: #32CC99; height: 20px; subcontrol-position: bottom; subcontrol-origin: margin; } QScrollBar::sub-line:vertical {border: 2px solid grey; background: #32CC99; height: 20px; subcontrol-position: top; subcontrol-origin: margin; } QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical { border: 2px solid grey;width: 3px; height: 3px; background: white; }QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {border: 2px solid grey; background: none;}");
+}
+
+TelloWidget::~TelloWidget()
+{
+    delete ui;
+}
+
+
