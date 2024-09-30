f47174565a41c346e2f7684fb47f7fba1f734fac
Erin <erinharrington12@yahoo.com>
2024-09-30 15:56:27 -0500

    Added Event Mode Handlers to Standard Widgets

--- include/botui/StandardWidget.h
+++ include/botui/StandardWidget.h
@@ -6,23 +6,30 @@
 class Device;
 class MenuBar;
 class StatusBar;
+class AboutWidget;
 
 class StandardWidget : public QWidget
 {
-Q_OBJECT
+	Q_OBJECT
 public:
 	StandardWidget(Device *device, QWidget *parent = 0);
 	~StandardWidget();
-	
+	void setupConnections(AboutWidget *aboutWidget);
+	bool getEventModeStateDefault();
+	void reloadStatusBar();
+	void deleteStatusBar();
 	Device *device() const;
-	
+
 protected:
-	void performStandardSetup(const QString& title, const bool &statusBar = true);
+	void performStandardSetup(const QString &title, const bool &statusBar = true);
 	void disableMenuBar();
 	void enableMenuBar();
+	void addEventModeLabel();
+	void removeEventModeLabel();
+	bool isEventModeEnabled();
 	MenuBar *menuBar() const;
 	StatusBar *statusBar() const;
-	
+
 private:
 	Device *m_device;
 	MenuBar *m_menuBar;
