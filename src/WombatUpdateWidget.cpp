810136e2c69843c53b304cc5b56e67c3eb67f7d4
Erin <erinharrington12@yahoo.com>
2024-11-15 11:12:35 -0600

    Temp file path for USB update file **will update to change .zip path**

--- src/WombatUpdateWidget.cpp
+++ src/WombatUpdateWidget.cpp
@@ -68,7 +68,7 @@ void WombatUpdateWidget::update()
       m_updateProc->setWorkingDirectory("/home/kipr");
       ui->updateConsole->setProcess(m_updateProc);
       connect(m_updateProc, SIGNAL(finished(int, QProcess::ExitStatus)), SLOT(updateFinished(int, QProcess::ExitStatus)));
-      m_updateProc->start("sh", QStringList() << "-c" << "/home/kipr/updateMe.sh /media/kipr/*/*");
+      m_updateProc->start("sh", QStringList() << "-c" << "/home/kipr/updateMe.sh /media/kipr/*/wombat-os-31Update.zip");
 
       // Update script will reboot the controller
     }
