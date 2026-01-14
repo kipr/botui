e3dd18a412f66364d51fda6a5892feeb68c584b6
Erin <erinharrington12@yahoo.com>
2026-01-14 15:43:09 -0600

    Changing run command to deal with console logs not flushing properly

--- src/ProgramsWidget.cpp
+++ src/ProgramsWidget.cpp
@@ -29,10 +29,10 @@ class ItemDelegate : public QItemDelegate
 {
 public:
 	ItemDelegate(QObject *parent = 0)
-		: QItemDelegate(parent)
+			: QItemDelegate(parent)
 	{
 	}
-	
+
 	virtual QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 	{
 		return QSize(0, 22);
@@ -40,13 +40,13 @@ public:
 };
 
 ProgramsWidget::ProgramsWidget(Device *device, QWidget *parent)
-	: StandardWidget(device, parent),
-	ui(new Ui::ProgramsWidget),
-	m_model(new ArchivesModel(device, this))
+		: StandardWidget(device, parent),
+			ui(new Ui::ProgramsWidget),
+			m_model(new ArchivesModel(device, this))
 {
 	ui->setupUi(this);
 	performStandardSetup(tr("Programs"));
-	
+
 	ui->programs->setItemDelegate(new ItemDelegate(this));
 	ui->programs->setModel(m_model);
 	connect(ui->run, SIGNAL(clicked()), SLOT(run()));
@@ -56,7 +56,7 @@ ProgramsWidget::ProgramsWidget(Device *device, QWidget *parent)
 	connect(ui->remove, SIGNAL(clicked()), SLOT(remove()));
 	connect(ui->args, SIGNAL(clicked()), SLOT(args()));
 	connect(ui->transfer, SIGNAL(clicked()), SLOT(transfer()));
-	
+
 	// TODO: remove these once the buttons all work
 	ui->edit->setVisible(true);
 	ui->add->setVisible(false);
@@ -66,12 +66,12 @@ ProgramsWidget::ProgramsWidget(Device *device, QWidget *parent)
 	ui->compile->setVisible(false);
 
 	connect(ui->programs->selectionModel(), SIGNAL(selectionChanged(QItemSelection, QItemSelection)),
-		SLOT(update()));
-	
-  QTimer *timer = new QTimer(this);
-  timer->start(2500);
-  connect(timer, SIGNAL(timeout()), SLOT(update()));
-  
+					SLOT(update()));
+
+	QTimer *timer = new QTimer(this);
+	timer->start(2500);
+	connect(timer, SIGNAL(timeout()), SLOT(update()));
+
 	update();
 }
 
@@ -82,21 +82,23 @@ ProgramsWidget::~ProgramsWidget()
 
 void ProgramsWidget::run()
 {
-  if(Program::instance()->isRunning()) return;
-  
+	if (Program::instance()->isRunning())
+		return;
+
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
-  
-  //ui->run->setEnabled(false);
-	
+	if (currents.size() != 1)
+		return;
+
+	// ui->run->setEnabled(false);
+
 	const QString name = m_model->name(currents[0]);
-	
-  // TODO: removed attempt to compile
+
+	// TODO: removed attempt to compile
 	/*if(!QFileInfo(SystemPrefix::ref().rootManager()->binPath(name)).exists()) {
 		qWarning() << "Could not find executable for" << name << ". Trying to compile.";
 		LogDialog logger;
-    const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
-  	kiss::KarPtr archive = kiss::Kar::load(archivePath);
+		const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
+		kiss::KarPtr archive = kiss::Kar::load(archivePath);
 		ConcurrentCompile compiler(name, archive, device());
 		compiler.setAutoDelete(false);
 		compiler.setUserData(&logger);
@@ -109,28 +111,29 @@ void ProgramsWidget::run()
 		QThreadPool::globalInstance()->start(&compiler);
 		RootController::ref().presentDialog(&logger);
 		if(!Compiler::Output::isSuccess(compiler.output())) {
-      ui->run->setEnabled(true);
-      return;
-    }
+			ui->run->setEnabled(true);
+			return;
+		}
 	}*/
 
-  // Make sure binary exists for this project
-  const QDir projDir(botui::pathToKISS + name);
-  qDebug() << name;
-  if(!projDir.exists("bin/botball_user_program")) {
-    QMessageBox::warning(this, tr("No Executable"), tr("No executable exists for the selected proejct."));
-    return;
-  }
-  
-  ui->run->setEnabled(false);
-  
-  // TODO: removed attempt to compile
-  /*const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
+	// Make sure binary exists for this project
+	const QDir projDir(botui::pathToKISS + name);
+	qDebug() << name;
+	if (!projDir.exists("bin/botball_user_program"))
+	{
+		QMessageBox::warning(this, tr("No Executable"), tr("No executable exists for the selected proejct."));
+		return;
+	}
+
+	ui->run->setEnabled(false);
+
+	// TODO: removed attempt to compile
+	/*const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
 	ProgramWidget *programWidget = new ProgramWidget(Program::instance(), device());
-  kiss::KarPtr archive = kiss::Kar::load(archivePath);
-  bool success = true;
+	kiss::KarPtr archive = kiss::Kar::load(archivePath);
+	bool success = true;
 	if(!(success = Program::instance()->start(SystemPrefix::ref().rootManager()->bin(name).filePath(name),
-      ProgramArguments::arguments(archive)))) {
+			ProgramArguments::arguments(archive)))) {
 		qWarning() << "Failed to start program" << name << ". Trying to compile.";
 		LogDialog logger;
 		ConcurrentCompile compiler(name, archive, device());
@@ -145,40 +148,50 @@ void ProgramsWidget::run()
 		QThreadPool::globalInstance()->start(&compiler);
 		RootController::ref().presentDialog(&logger);
 		success = Compiler::Output::isSuccess(compiler.output());
-    if(success) success = Program::instance()->start(SystemPrefix::ref().rootManager()->bin(name).filePath(name),
-      ProgramArguments::arguments(archive));
+		if(success) success = Program::instance()->start(SystemPrefix::ref().rootManager()->bin(name).filePath(name),
+			ProgramArguments::arguments(archive));
 	}*/
-  
-  ProgramWidget *programWidget = new ProgramWidget(Program::instance(), device());
-  const bool success = Program::instance()->start(projDir.filePath("bin/botball_user_program"), QStringList());
-  
-  if(success) RootController::ref().presentWidget(programWidget);
-  else delete programWidget;
-  
-  ui->run->setEnabled(true);
+	const QString programPath = projDir.filePath("bin/botball_user_program");
+
+	// Shell-safe single-quote escaping:  abc'd  ->  'abc'"'"'d'
+	QString escaped = programPath;
+	escaped.replace("'", "'\"'\"'");
+
+	const QString cmd = QString("'%1'").arg(escaped);
+
+	ProgramWidget *programWidget = new ProgramWidget(Program::instance(), device());
+	const bool success = Program::instance()->start(
+			"/usr/bin/script",
+			QStringList({"-q", "-c", cmd, "/dev/null"}));
+	if (success)
+		RootController::ref().presentWidget(programWidget);
+	else
+		delete programWidget;
+
+	ui->run->setEnabled(true);
 }
 
 void ProgramsWidget::edit()
 {
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
-	
-  
+	if (currents.size() != 1)
+		return;
+
 	const QString name = m_model->name(currents[0]);
 	qDebug() << "edit clicked for " << name;
 
-    //const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
-    //qDebug() << "archivePath" << archivePath;
-	//kiss::KarPtr archive = kiss::Kar::load(archivePath);
-	//if(archive.isNull()) return;
+	// const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
+	// qDebug() << "archivePath" << archivePath;
+	// kiss::KarPtr archive = kiss::Kar::load(archivePath);
+	// if(archive.isNull()) return;
 
 	const QString projectPath = botui::pathToKISS + name;
 	qDebug() << "project path: " << projectPath;
-	
+
 	EditorWidget *editor = new EditorWidget(device());
 	editor->setProjectPath(projectPath);
-	//editor->setArchive(archive);
-	//editor->setSavePath(archivePath);
+	// editor->setArchive(archive);
+	// editor->setSavePath(archivePath);
 
 	RootController::ref().presentWidget(editor);
 }
@@ -186,36 +199,39 @@ void ProgramsWidget::edit()
 void ProgramsWidget::add()
 {
 	KeyboardDialog keyboard(tr("Program Name"));
-	if(RootController::ref().presentDialog(&keyboard) != QDialog::Accepted) return;
+	if (RootController::ref().presentDialog(&keyboard) != QDialog::Accepted)
+		return;
 	const QString name = keyboard.input();
-	if(name.isEmpty()) return;
-	
+	if (name.isEmpty())
+		return;
+
 	/* FIXME
 	const QString archivePath = SystemPrefix::ref().rootManager()->archivesPath(name);
 
-    if(!kiss::Kar::load(archivePath).isNull()) {
+		if(!kiss::Kar::load(archivePath).isNull()) {
 		AreYouSureDialog dialog;
 		dialog.setDescription(tr("You're about to overwrite the program \"%1\". Continue?\n").arg(name));
 		if(RootController::ref().presentDialog(&dialog) != QDialog::Accepted) return;
 	}
-	
+
 	kiss::KarPtr archive = kiss::Kar::create();
 	if(!archive->save(archivePath)) {
 		qWarning() << "Failed to create new archive with the name" << name;
 		return;
 	}
-	
+
 	*/
 	EditorWidget *editor = new EditorWidget(device());
-	//FIXME: editor->setArchive(archive);
-	//FIXME: editor->setSavePath(archivePath);
+	// FIXME: editor->setArchive(archive);
+	// FIXME: editor->setSavePath(archivePath);
 	RootController::ref().presentWidget(editor);
 }
 
 void ProgramsWidget::args()
 {
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
+	if (currents.size() != 1)
+		return;
 	const QString name = m_model->name(currents[0]);
 	RootController::ref().presentWidget(new ProgramArgsWidget(name, device()));
 }
@@ -223,21 +239,21 @@ void ProgramsWidget::args()
 void ProgramsWidget::compile()
 {
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
+	if (currents.size() != 1)
+		return;
 
-  	const QString name = m_model->name(currents[0]);
+	const QString name = m_model->name(currents[0]);
 
-  	qDebug() << "compile clicked for " << name;
+	qDebug() << "compile clicked for " << name;
 
 	const QString projectPath = botui::pathToKISS + name;
-
 
 	const QDir includeDir(projectPath + "/include/");
 	const QDir srcDir(projectPath + "/src/");
 
 	QString binFilePath = projectPath + "bin/botball_user_program";
 
-        QString compileCommand = "gcc -o " + binFilePath + " -lwallaby -lpthread -lm -I " + includeDir.absolutePath() + " " + srcDir.absolutePath() + "/*.c";
+	QString compileCommand = "gcc -o " + binFilePath + " -lwallaby -lpthread -lm -I " + includeDir.absolutePath() + " " + srcDir.absolutePath() + "/*.c";
 	qDebug() << compileCommand;
 
 	QByteArray ba = compileCommand.toLatin1();
@@ -245,58 +261,62 @@ void ProgramsWidget::compile()
 
 	int ret = std::system(compileCommandC);
 
-
 	qDebug() << "ret = " << ret;
 }
 
 void ProgramsWidget::remove()
 {
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
+	if (currents.size() != 1)
+		return;
 	SystemPrefix::ref().rootManager()->uninstall(m_model->name(currents[0]));
 	update();
 }
- 
+
 void ProgramsWidget::transfer()
 {
 	QModelIndexList currents = ui->programs->selectionModel()->selectedIndexes();
-	if(currents.size() != 1) return;
-  const QString name = m_model->name(currents[0]);
+	if (currents.size() != 1)
+		return;
+	const QString name = m_model->name(currents[0]);
 	/* FIXME: const kiss::KarPtr archive = kiss::Kar::load(SystemPrefix::ref().rootManager()->archivesPath(name));
-  const QDir flashDrive("/kovan/media/sda1/transfers/" + name);
-  QDir().mkpath(flashDrive.path());
-  archive->extract(flashDrive.path());
-  */
+	const QDir flashDrive("/kovan/media/sda1/transfers/" + name);
+	QDir().mkpath(flashDrive.path());
+	archive->extract(flashDrive.path());
+	*/
 }
 
 void ProgramsWidget::compileStarted(const QString &name, ConcurrentCompile *compiler)
 {
-	if(!compiler) {
+	if (!compiler)
+	{
 		qWarning() << "sender is null";
 		return;
 	}
-	
+
 	LogDialog *log = reinterpret_cast<LogDialog *>(compiler->userData());
 	log->setStatus(tr("Compiling..."));
 }
 
 void ProgramsWidget::compileFinished(const Compiler::OutputList &output, ConcurrentCompile *compiler)
 {
-	if(!compiler) {
+	if (!compiler)
+	{
 		qWarning() << "sender is null";
 		return;
 	}
 
 	LogDialog *log = reinterpret_cast<LogDialog *>(compiler->userData());
-	foreach(const Compiler::Output& out, output) {
+	foreach (const Compiler::Output &out, output)
+	{
 		log->appendText(CompileHelpers::postProcess(out.output()));
 		log->appendText(CompileHelpers::postProcess(out.error()));
 	}
-	
+
 	// This is more so something will be in the log.
 	// It's disconcerting when it's entirely empty.
 	log->appendText(tr("Compile finished."));
-	
+
 	log->setStatus(Compiler::Output::isSuccess(output) ? tr("Success!") : tr("Failure."));
 	log->setDismissable(true);
 }
@@ -309,6 +329,6 @@ void ProgramsWidget::update()
 	ui->edit->setEnabled(good);
 	ui->remove->setEnabled(good);
 	ui->args->setEnabled(good);
-  const QDir flashDrive("/kovan/media/sda1");
+	const QDir flashDrive("/kovan/media/sda1");
 	ui->transfer->setEnabled(good && flashDrive.exists());
 }
